1a6fc405e43e571aa92f289aaf3834276551f036
Gx <gomaxlou@gmail.com>
2020-12-29 10:58:39 +0800

    Toast, AlertDialog, 事件處理, 多國語言

--- app/src/main/java/edu/gx/bmi/MainActivity.kt
+++ app/src/main/java/edu/gx/bmi/MainActivity.kt
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        b_help.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("BMI 說明")
+                .setMessage("體重(kg)/身高平方(m)")
+                .setPositiveButton("OK", null)
+                .show()
+        }
     }
 
     fun bmi(view: View) {
@@ -17,5 +26,18 @@ class MainActivity : AppCompatActivity() {
         val height = ed_height.text.toString().toFloat()
         val bmi = weight / (height * height)
         Log.d("BMI", bmi.toString())
+        Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
+//        val builder = AlertDialog.Builder(this)
+//        builder.setMessage(bmi.toString())
+//        builder.show()
+        AlertDialog.Builder(this)
+            .setMessage(bmi.toString())
+            .setTitle("BMI:")
+            .setPositiveButton("OK", null)  // 正向
+            .setNeutralButton("CANCEL", null) // 中性
+            .setNegativeButton("ERROR", null)   // 反向
+            .show()
+
     }
+
 }
